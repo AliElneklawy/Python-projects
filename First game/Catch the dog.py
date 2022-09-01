476ff4f6ac9bf215097a3f0a4ddb30d8267d2a90
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-09-01 18:22:15 +0200

    Update Catch the dog.py

--- First game/Catch the dog.py
+++ First game/Catch the dog.py
@@ -13,9 +13,9 @@ my_color = (10, 100, 200)
 width = 1000
 height = 700
 frames_per_second = 60
-ball_width_height = 90 # pixels
-max_width = width - ball_width_height
-max_height = height - ball_width_height
+chaser_width_height = 90 # pixels
+max_width = width - chaser_width_height
+max_height = height - chaser_width_height
 target_width_height = 120
 npixels_to_move = 10
 
@@ -25,17 +25,17 @@ window = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 # load assets
-ball = pygame.image.load(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\chaser.png")
+chaser = pygame.image.load(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\chaser.png")
 target = pygame.image.load(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\target2.png")
 bounceSound = pygame.mixer.Sound(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\Cartoon Bounce Sound Effect.wav")
 backgroundPic = pygame.image.load(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\backgroung.jpg")
-backgroundSound = pygame.mixer.music.load(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\Chapter_5_PygameDemo4_OneBallBounce_sounds_background.mp3")
+backgroundSound = pygame.mixer.music.load(r"C:\Users\M E T R O\OneDrive\Desktop\Study\python\6 exercise\Chapter_5_PygameDemo4_OneballBounce_sounds_background.mp3")
 pygame.mixer.music.play(-1, 0, 0)
 
 # initialize the variables
-ballX = randrange(max_width)
-ballY = randrange(max_height)
-ballRect = pygame.Rect(ballX, ballY, ball_width_height, ball_width_height)
+chaserX = randrange(max_width)
+chaserY = randrange(max_height)
+chaserRect = pygame.Rect(chaserX, chaserY, chaser_width_height, chaser_width_height)
 target_x = randrange(max_width)
 target_y = randrange(max_height)
 target_y = 320
@@ -48,32 +48,32 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEBUTTONUP:  # if this event's occurs, pygame creates an event object containing alot of data
-            ballX = randrange(max_width)
-            ballY = randrange(max_height)
-            ballRect = pygame.Rect(ballX, ballY, ball_width_height, ball_width_height)
+            chaserX = randrange(max_width)
+            chaserY = randrange(max_height)
+            chaserRect = pygame.Rect(chaserX, chaserY, chaser_width_height, chaser_width_height)
 
     # per frame action
     keyPressedTuple = pygame.key.get_pressed()  # this return which key is currently down at each frame. 0 if key up 1 if key down
     if keyPressedTuple[pygame.K_LEFT]:
-        ballX -= npixels_to_move
+        chaserX -= npixels_to_move
     elif keyPressedTuple[pygame.K_RIGHT]:
-        ballX += npixels_to_move
+        chaserX += npixels_to_move
     elif keyPressedTuple[pygame.K_UP]:
-        ballY -= npixels_to_move
+        chaserY -= npixels_to_move
     elif keyPressedTuple[pygame.K_DOWN]:
-        ballY += npixels_to_move
+        chaserY += npixels_to_move
 
-    if ballY < 0:
-        ballY = max_height
-    elif ballY > height:
-        ballY = 0
-    elif ballX < 0:
-        ballX = max_width
-    elif ballX > max_width:
-        ballX = 0
-    ballRect = pygame.Rect(ballX, ballY, ball_width_height, ball_width_height)
+    if chaserY < 0:
+        chaserY = max_height
+    elif chaserY > height:
+        chaserY = 0
+    elif chaserX < 0:
+        chaserX = max_width
+    elif chaserX > max_width:
+        chaserX = 0
+    chaserRect = pygame.Rect(chaserX, chaserY, chaser_width_height, chaser_width_height)
 
-    if ballRect.colliderect(targetRect):
+    if chaserRect.colliderect(targetRect):
         bounceSound.play()
         target_x = randrange(max_width)
         target_y = randrange(max_height)
@@ -82,6 +82,6 @@ while True:
     # window.fill(my_color)
     window.blit(backgroundPic, (0, 0))
     window.blit(target, (target_x, target_y))
-    window.blit(ball, (ballX, ballY))
+    window.blit(chaser, (chaserX, chaserY))
     pygame.display.update()
     clock.tick(frames_per_second) # pause
